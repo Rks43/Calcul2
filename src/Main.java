8116a0ae889a10787728299e6517d3776ad38187
Your Name <your_email@whatever.com>
2023-08-03 16:34:20 +0300

    Add method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public int
+    сalculate(int x, int y, char expression) {
+        int z = 0;
+        try {
+            switch (expression) {
+                case '+': z = x+y; break;
+                case '-': z = x-y; break;
+                case '*': z = x*y; break;
+                case '/': z = x/y; break;
+            }
+            return z;
+        }
+        catch (Exception ex)
+        {
+            return 0;
+        }
+    }
+    public void main(String[] args)
+    {
+        int t = сalculate(5,5,'+');
+        System.out.println(t);
     }
 }
