ebdd5ba35078c32664d6293b1a1a4a67132bde64
Your Name <your_email@whatever.com>
2023-08-03 16:46:03 +0300

    Add class

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,35 @@
 
 
 public class Main {
-    public int
-    сalculate(int x, int y, char expression) {
-        int z = 0;
-        try {
-            switch (expression) {
-                case '+': z = x+y; break;
-                case '-': z = x-y; break;
-                case '*': z = x*y; break;
-                case '/': z = x/y; break;
-            }
-            return z;
-        }
-        catch (Exception ex)
-        {
-            return 0;
-        }
-    }
+   class Calculator {
+       public int
+       сalculate(int x, int y, char expression) {
+           int z = 0;
+           try {
+               switch (expression) {
+                   case '+':
+                       z = x + y;
+                       break;
+                   case '-':
+                       z = x - y;
+                       break;
+                   case '*':
+                       z = x * y;
+                       break;
+                   case '/':
+                       z = x / y;
+                       break;
+               }
+               return z;
+           } catch (Exception ex) {
+               return 0;
+           }
+       }
+   }
     public void main(String[] args)
     {
-        int t = сalculate(5,5,'+');
+        Calculator c1 = new Calculator();
+        int t = c1.сalculate(5, 5, '+');
         System.out.println(t);
     }
 }
